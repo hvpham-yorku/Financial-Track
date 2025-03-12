e6f7f5cebded8628f3c6949e7a82846a25c3169e
Mathieu Johnson <mathieujohnson10@gmail.com>
2025-03-12 14:39:28 -0400

    Add auth and transaction api routes + auth middleware

--- FinancialTrack/server/src/config.ts
+++ FinancialTrack/server/src/config.ts
@@ -5,6 +5,7 @@ dotenv.config();
 
 export class Config {
   public static PORT = process.env.PORT || 3000;
+  public static SECRET = process.env.JWT_SECRET!;
   public static DB = "src/db/financialtrack.sqlite";
   public static DB_DIALECT: Dialect = "sqlite";
 }
---
+++ FinancialTrack/server/src/routes/auth.ts
@@ -0,0 +1,71 @@
+import { Router } from "express";
+import { Request, Response } from "express";
+import User from "../models/User";
+import { Config } from "../config";
+import jwt from "jsonwebtoken";
+
+const AuthRoute = Router();
+
+AuthRoute.get("/", async (_, res: Response) => {
+  const users = await User.findAll();
+  res.json(users);
+});
+
+AuthRoute.post("/", async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  const user = await User.create({ username, password });
+  res.json(user);
+});
+
+AuthRoute.post("/register", async (req: Request, res: Response) => {
+  try {
+    // Check if the email already exists
+    const existingUser = await User.findOne({
+      where: { username: req.body.username },
+    });
+    if (existingUser) {
+      res.status(400).json({ error: "Username already exists" });
+      return;
+    }
+
+    const user = await User.create({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    const token = jwt.sign(
+      { username: user.username, id: user.id },
+      Config.SECRET,
+      { expiresIn: "7d" }
+    );
+    res.status(201).json({ message: "User registered successfully", token });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+AuthRoute.post("/login", async (req: Request, res: Response) => {
+  try {
+    // Check if the email exists
+    const user = await User.findOne({ where: { username: req.body.username } });
+    if (!user) {
+      res.status(401).json({ error: "Invalid credentials" });
+      return;
+    }
+    if (user.password !== req.body.password) {
+      res.status(401).json({ error: "Invalid credentials" });
+      return;
+    }
+
+    // Generate JWT token
+    const token = jwt.sign(
+      { username: user.username, id: user.id },
+      Config.SECRET,
+      { expiresIn: "7d" }
+    );
+    res.status(200).json({ message: "User logged in successfully", token });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+export default AuthRoute;
