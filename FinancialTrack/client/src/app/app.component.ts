3387f21d33aacc2996ee854b12f2faf3d254a40f
Yash0407 <yasvir.ys.singh@gmail.com>
2025-03-13 18:15:54 -0400

    Implemented home page part1.

--- FinancialTrack/client/src/app/app.component.ts
+++ FinancialTrack/client/src/app/app.component.ts
@@ -4,20 +4,29 @@ import { User } from './models/user.model';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { log } from 'console';
+import { ButtonModule } from 'primeng/button';
+import { TabsModule } from 'primeng/tabs';
+import { DatePicker, DatePickerModule } from 'primeng/datepicker';
+import { FloatLabel } from 'primeng/floatlabel';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HttpClientModule, FormsModule],
+  imports: [HttpClientModule, FormsModule, ButtonModule, TabsModule, DatePickerModule, FloatLabel],
   providers: [UserService],
 })
 export class AppComponent {
   username: string = '';
   password: string = '';
   ID = 0;
-  constructor(private userService: UserService) {}
+  monthlyDate: any;
+  weeklyDate:any;
+
+  constructor(private userService: UserService) {
+    this.monthlyDate = new Date();
+  }
 
   addUser() {
     const newUser = new User(this.username, this.password, this.ID++);
