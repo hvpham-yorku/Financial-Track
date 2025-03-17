f7f0ac0428d57eca45849261e03b111663e4551a
Yash0407 <yasvir.ys.singh@gmail.com>
2025-03-16 23:57:44 -0400

    Frontend implementation v2: transactions & homepage

--- FinancialTrack/client/src/app/app.config.ts
+++ FinancialTrack/client/src/app/app.config.ts
@@ -11,7 +11,7 @@ export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideAnimationsAsync(),
     providePrimeNG({
         theme: {
-            preset: Indigo
+            preset: Indigo,
         }
     }),
     provideHttpClient(withFetch()),
